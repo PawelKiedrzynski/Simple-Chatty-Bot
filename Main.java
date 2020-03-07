7b540af3efcb34634de5ce81f6fbae99bf601c0e
Paweł Kiedrzyński <42970221+PawelKiedrzynski@users.noreply.github.com>
2020-03-07 10:15:40 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package pl.pk.java.simplechattybot;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleBot simpleBot = new SimpleBot();
+        simpleBot.greet("Jarvis", "2020");
+        simpleBot.remindName();
+        simpleBot.guessAge();
+        simpleBot.count();
+        simpleBot.test();
+        simpleBot.end();
+    }
+
+}
