18848389715cba7d58521742200b66f4dfff71a2
Paweł Kiedrzyński <42970221+PawelKiedrzynski@users.noreply.github.com>
2020-03-07 10:24:27 +0100

    Change Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,7 @@ package pl.pk.java.simplechattybot;
 public class Main {
     public static void main(String[] args) {
         SimpleBot simpleBot = new SimpleBot();
-        simpleBot.greet("Jarvis", "2020");
-        simpleBot.remindName();
-        simpleBot.guessAge();
-        simpleBot.count();
-        simpleBot.test();
-        simpleBot.end();
+        simpleBot.run();
     }
 
 }
